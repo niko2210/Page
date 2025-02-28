76e8c0951eff0a344e80e768fc4ec0ce92076b97
niko2210 <nkk88@live.dk>
2025-03-01 00:06:12 +0100

    setup for server side programming

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+console.log('running');
+
 function showMessage() {
     document.getElementById("message").innerText = "Hello from GitHub Pages! 🚀";
 }
