62ad13b6214402eadbaae75c5d12e6b00d50d817
niko2210 <nkk88@live.dk>
2025-03-24 17:46:11 +0100

    ,

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log('running');
 
 function showMessage() {
     document.getElementById("message1").innerText = "Hello from GitHub Pages! 🚀";
-    document.getElementById("message2").innerText = "Hello from GitHub Pages! 🚀";
+    document.getElementById("message2").innerText = "your mom";
 }
 
 
